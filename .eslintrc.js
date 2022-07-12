331ec551846436735d3f46c34a0f58853543327b
VickeyFeng <vvkkfwq@sina.com>
2022-07-13 00:45:30 +0800

    chore: 优化eslint配置文件的配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,24 +7,25 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-essential',
     'airbnb-base',
-    'prettier',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
+  parser: 'vue-eslint-parser',
   parserOptions: {
     ecmaVersion: 'latest',
     parser: '@typescript-eslint/parser',
     sourceType: 'module',
   },
-  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  plugins: ['vue', '@typescript-eslint'],
   rules: {
-    // 使用airbnb规范时会提示eslint应在依赖中而不是开发依赖中，下面以禁用警告
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+    // 组件命名
     'vue/multi-word-component-names': [
       'error',
       {
         ignores: ['index'], // 需要忽略的组件名
       },
     ],
+    // 文件后缀
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -37,6 +38,7 @@ module.exports = {
     ],
   },
   settings: {
+    // 识别别名
     'import/resolver': {
       typescript: {
         directory: './tsconfig.json',
