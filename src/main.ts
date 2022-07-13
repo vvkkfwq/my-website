5ff86a883d3f2fdb9b15b29583f4f7ee3e4a1cb5
VickeyFeng <vvkkfwq@sina.com>
2022-07-13 23:55:21 +0800

    chore: 引入Pinia

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import Router from './router';
+import store from './store';
 
 const app = createApp(App);
 
 // 挂载Router
 app.use(Router);
+
+// 挂载pinia
+app.use(store);
 
 app.mount('#app');
